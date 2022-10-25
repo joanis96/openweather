40053443943448d7146af65ff1a490d8a7528233
joanis96 <57779348+joanis96@users.noreply.github.com>
2022-10-25 12:11:56 -0400

    changement mineur

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // api key : 3d32ffd95e9094cbdc2a01b3989094c8
+
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Server is up and running.");
+})
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+})
